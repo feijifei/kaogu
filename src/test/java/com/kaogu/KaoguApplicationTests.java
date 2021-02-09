2833a1f47bb7551a51a4c68ade8aee9fe623f7fc
叶晟杰 <11710524@mail.sustech.edu.cn>
2021-02-10 01:44:58 +0800

    another update

---
+++ src/test/java/com/kaogu/KaoguApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaogu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaoguApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
